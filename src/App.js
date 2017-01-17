829e8ac57a61f42b536a6bc8c9810422306d117f
Alexander Kallaway <kallaway@me.com>
2017-01-17 00:52:34 -0500

    start work on the app

--- src/App.js
+++ src/App.js
@@ -8,14 +8,54 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>FreeCodeCamp Leaderboard</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+		<Leaderboard />
       </div>
     );
   }
+}
+
+class Leaderboard extends Component {
+	constructor(props) {
+		super(props);
+
+	}
+	// on load, get the information
+
+	// get the info on all the campers
+
+
+
+	render() {
+
+		// var campers = this.props.data.map(function(camper) {
+		// 	return <Camper />;
+		// });
+		// {campers}
+		return <table>
+			<tr>
+				<td>Leaderboard</td>
+			</tr>
+			<tr></tr>
+
+		</table>
+	}
+}
+
+class Camper extends Component {
+	render() {
+		return <tr>
+			<td className="number"></td>
+			<td className="name"></td>
+			<td className="points30"></td>
+			<td className="pointsAll"></td>
+		</tr>
+	}
 }
 
 export default App;
