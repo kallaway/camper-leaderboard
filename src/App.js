df3657b3d4e59a70fbc952daf146631abd9756c1
Alexander Kallaway <kallaway@me.com>
2017-01-19 22:57:29 -0500

    working on fetching and displaying data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   render() {
@@ -28,7 +29,32 @@ class Leaderboard extends Component {
 	// on load, get the information
 
 	// get the info on all the campers
+	// getData: function() {
+	// 	var days30 = axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent').then(function(camper) {
+	// 		this.props.data.days30 = {
+	// 			username: camper.username,
+	// 			img: camper.img,
+	// 			alltime: camper.alltime,
+	// 			recent: camper.recent,
+	// 			lastUpdate: camper.lastUpdate
+	// 		}
+	// 	});
+	// 	var allTime = axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime').then(function(camper) {
+	// 		this.props.data.allTime = {
+	// 			username: camper.username,
+	// 			img: camper.img,
+	// 			alltime: camper.alltime,
+	// 			recent: camper.recent,
+	// 			lastUpdate: camper.lastUpdate
+	// 		}
+	// 	});;
+	// }
 
+	componentDidMount() {
+		// getData();
+		this.props.data.dataReceived = axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime');
+		console.log(axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime'));
+	}
 
 
 	render() {
@@ -41,7 +67,18 @@ class Leaderboard extends Component {
 			<tr>
 				<td>Leaderboard</td>
 			</tr>
-			<tr></tr>
+			<tr>
+				{this.props.data.dataReceived.map(function(camper) {
+					return (
+						<tr>
+							<td>Username: {camper.username}</td>
+							<td>Img: <img src={camper.img} /></td>
+							<td>AllTime: {camper.allTime}</td>
+						</tr>
+					);
+				})
+				}
+			</tr>
 
 		</table>
 	}
